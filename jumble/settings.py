67cd147253a8cd3c575cd5f870a16ba1e5b0dfb5
Somdutta Sinha <S.Sinha-5@student.tudelft.nl>
2023-08-13 12:59:05 +0200

    [infra] rename app to api

--- jumble/settings.py
+++ jumble/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y!=#xezhl+1_+j-(wnz*ymxxcw_29z-+u12i*#(me_um_!+)k#'
+SECRET_KEY = 'django-insecure-ee&eaioc2@-c3tm194k514gah#2)kk-rnqrd93k_jdbcpq1v1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
